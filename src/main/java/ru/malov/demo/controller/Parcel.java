4759e6a40507e0176bf5a98cbed3b0f46780bfbb
AlexsanderM <malov.alexsander@yandex.ru>
2020-09-24 22:45:54 +0200

    NotFound exception

--- src/main/java/ru/malov/demo/controller/Parcel.java
+++ src/main/java/ru/malov/demo/controller/Parcel.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.malov.demo.exception.NotFoundException;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -30,6 +31,6 @@ public class Parcel {
         return list.stream()
                 .filter(l -> l.get("id").equals(id))
                 .findFirst()
-                .orElseThrow();
+                .orElseThrow(NotFoundException::new);
     }
 }
